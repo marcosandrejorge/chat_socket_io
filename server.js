8624333b620084c468af7d2d5015f44bb58a51ed
Marcos André Jorge <marcosjorge@unimestre.com>
2020-06-16 13:56:19 -0300

    Ciração de salas

--- server.js
+++ server.js
@@ -19,13 +19,27 @@ let messages = [];
 io.on('connection', socket => {
    console.log(`Id conectado:${socket.id}`);
 
-   //Emit só para o socket conectado;
-   socket.emit('previousMessage', messages);
+   socket.on('addToRoom', data => {
+      //Entra com o usuário na sala selecionada;
+      socket.join(data);
+
+      //Recupera todas as mensagens da sala que o usuário acabou de entrar
+      let arrMessagesSala = messages.filter(message => {
+         return message.room == data;
+      })
+
+      //Emit para o usuário com todas as mensagens da sala;
+      socket.emit('previousMessage', arrMessagesSala);
+   });
 
    socket.on('sendMessage', data => {
       messages.push(data);
-      //Emit só para todos os sockets conectados;
-      socket.broadcast.emit('receivedMessage', data);
+      //Emit para todos os sockets conectados a sala que o usuário está - Não emit o evento para o usuário atual só para o restante da sala;
+      socket.broadcast.to(data.room).emit('receivedMessage', data);
+   });
+
+   socket.on('removeFromRoom', data => {
+      socket.leave(data);
    });
 });
 
--- src/function.js
+++ src/function.js
@@ -1,4 +1,24 @@
 $(document).ready(function() {
+
+   showConectar();
+
+   $('#conectar').click(function () {
+      var room = $('#room').val();
+
+      if (!room.length) return;
+
+      socket.emit('addToRoom', room);
+      showSair();
+   });
+
+   $('#sair').click(function () {
+      var room = $('#room').val();
+      //Remove todas as menssagens anteriores.
+      $('.message').remove();
+      socket.emit('removeFromRoom', room);
+      showConectar();
+   });
+
    var socket = io('http://localhost:3000');
 
    function renderMessage(objMessage) {
@@ -7,17 +27,29 @@ $(document).ready(function() {
       );
    }
 
-   $('#chat').submit(function(event){
+   function showConectar() {
+      $('.div_sair').hide();
+      $('.div_conectar').show();
+   }
+
+   function showSair() {
+      $('.div_conectar').hide();
+      $('.div_sair').show();
+   }
+
+   $('#chat').submit(function(event) {
       event.preventDefault();
 
       var author = $('#username').val();
       var message = $('#message').val();
+      var room = $('#room').val();
 
       if (!author.length || !message.length) return;
 
       let objMessage = {
          author,
-         message
+         message,
+         room
       };
 
       renderMessage(objMessage);
